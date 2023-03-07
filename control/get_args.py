d5fb6f417fccb185e41afc266fa9ddc44bde08cf
Ilya.devel <ilya.devel@yandex.ru>
2023-03-07 13:44:30 +0500

    set_args complete (argument sorting)

--- control/get_args.py
+++ control/get_args.py
@@ -9,5 +9,8 @@ def get_args(args: list):
     """
     if len(args) == 0 or '--help' in args:
         return dict()
+    elif '--add' or '--show' or '--find' or '--edit' or '--delete' or '--export' or '--import' not in args:
+        print('UNKNOWN ARGUMENTS\n')
+        return dict()
     else:
         return set_args(args)
--- control/set_args.py
+++ control/set_args.py
@@ -1,3 +1,29 @@
 def set_args(args: list):
+    """
+    Function for arguments handling
+    :param args: list of arguments
+    :return:
+    """
+    lst_args = dict()
+    main_key = ''
+    tmp_key = ''
+    for i in range(len(args)):
 
-    return 0
+        if args[i] in ['--add', '--show', '--find', '--edit', '--delete', '--export', '--import']:
+            if tmp_key != '':
+                lst_args[main_key][tmp_key] = lst_args[main_key][tmp_key].strip()
+            main_key = args[i]
+            lst_args[main_key] = dict()
+            tmp_key = ''
+            if main_key in ['--find', '--edit']:
+                tmp_key = main_key
+                lst_args[main_key][tmp_key] = ''
+            continue
+
+        if args[i].startswith('--'):
+            tmp_key = args[i]
+            lst_args[main_key][tmp_key] = ''
+        else:
+            lst_args[main_key][tmp_key] += f'{args[i]} '
+
+    return lst_args
